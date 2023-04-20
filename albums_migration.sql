d996af62bda11fac338be2d8ae803162965a1365
RileyWood9984 <rjw9984@gmail.com>
2023-04-20 11:04:16 -0500

    albums done

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50),
                         record_name VARCHAR(100) NOT NULL,
-                        release_date DATE NOT NULL,
+                        release_date INT UNSIGNED NOT NULL,
                         sales FLOAT UNSIGNED NOT NULL,
                         genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
---
+++ select_exercises.sql
@@ -0,0 +1,3 @@
+USE codeup_test_db;
+    INSERT INTO albums(artist, record_name,realease_date,sales,genre)
+    VALUES ('pink')
