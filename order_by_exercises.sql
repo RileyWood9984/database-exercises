9a459d2f8ce67cf9930a65d0246d22681925e386
RileyWood9984 <rjw9984@gmail.com>
2023-04-24 10:51:03 -0500

    done

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,17 +2,19 @@ USE employees;
 SELECT emp_no,first_name,last_name
 FROM employees
 WHERE first_name='Irena'
-OR first_name='Vidya'
-OR first_name='Maya';
+   OR first_name='Vidya'
+   OR first_name='Maya'
+ORDER BY last_name desc, first_name desc;
 SELECT emp_no,first_name,last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-OR last_name LIKE '%e';
+   OR last_name LIKE '%e'
+ORDER BY emp_no desc;
 SELECT emp_no,first_name,last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-AND last_name LIKE '%e';
+  AND last_name LIKE '%e';
 SELECT emp_no,first_name,last_name
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+  AND last_name NOT LIKE '%qu%';
