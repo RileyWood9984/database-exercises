7f6bd369502724ec3ea0e4631cdc2a038711778f
RileyWood9984 <rjw9984@gmail.com>
2023-04-24 10:34:26 -0500

    done with part2

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,17 @@
 USE employees;
 SELECT emp_no,first_name,last_name
 FROM employees
-WHERE first_name IN('Irena','Vidya','Maya');
+WHERE first_name='Irene'
+OR first_name='Vidya'
+OR first_name='Maya';
 SELECT emp_no,first_name,last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 OR last_name LIKE '%e';
+SELECT emp_no,first_name,last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e';
 SELECT emp_no,first_name,last_name
 FROM employees
 WHERE last_name LIKE '%q%'
