e0b249a9953198d1dfd665f0666a050a6d866152
RileyWood9984 <rjw9984@gmail.com>
2023-04-25 15:51:16 -0500

    done wiht bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -46,7 +46,7 @@ ORDER BY d.dept_name;
 
 SELECT CONCAT(e.first_name,' ',e.last_name)
 AS `Employee`,d.dept_name
-AS 'Department',CONCAT(e.first_name,' ',e.last_name)
+AS 'Department',CONCAT(emp.first_name,' ',emp.last_name)
 AS `Manager`
 FROM employees AS e
 JOIN dept_emp AS de
@@ -55,6 +55,8 @@ JOIN departments AS d
 ON de.dept_no = d.dept_no
 JOIN dept_manager AS dm
 ON de.dept_no = dm.dept_no
+JOIN employees as emp
+ON dm.emp_no=emp.emp_no
 WHERE de.to_date='9999-01-01'
 AND dm.to_date='9999-01-01'
 order by d.dept_no desc;
